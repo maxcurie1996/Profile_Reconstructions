0e816899d4e04734db89cc4c4764d4fdb2c46350
maxcurie1996 <47543965+maxcurie1996@users.noreply.github.com>
2021-03-25 20:46:39 -0500

    Kinda works

--- 0run_freq_find.py
+++ 0run_freq_find.py
@@ -1,19 +1,130 @@
 import numpy as np 			#import numpy library and give it a short name as np
 import sys as sys
+import matplotlib.pyplot as plt
+
 sys.path.insert(1, '/u/mcurie/Scripts/tokaml-JeffsBranch/SULI2021/random_tools')
 from DataPrep import *
+from tools import data_labeler
+
+#Take the suggestion from Jeff
+
+#Created by Max Curie 3/25/2021
 
 #python /u/mcurie/Scripts/tokaml-JeffsBranch/0run_freq_find.py
 #shot_dir='/p/datad2/gdong/signal_data_ELM/d3d/d3d/B3'
 shot_dir='.'
 #for current folder use '.'
-shot_no='170882.pq'
+shot_no=170882
 
-a=DataPrep(shot_dir,shot_no)
+#shot = 170882 # this is the shot number you're looking at
+sh = DataPrep(shot_dir, shot_no) # this creates the DataPrep Object
+properties = sh.peak_properties()
+window_num = 1 # this is the specific window you're looking for. For a list of available window_num, do:
+#print(properties.index.get_level_values(level=0))
 
-a.elm_loc(plot=False,csv_output=True)#Locate the ELM
 
-a.make_mask(plot=False,csv_output=True)
+#From https://stackabuse.com/writing-to-a-file-with-pythons-print-function/
+original_stdout = sys.stdout # Save a reference to the original standard output
+with open('output.txt', 'w') as f:
+	sys.stdout = f # Change the standard output to the file we created.
+	#From: https://stackoverflow.com/questions/19124601/pretty-print-an-entire-pandas-series-dataframe
+	with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+		print(properties)
+	sys.stdout = original_stdout # Reset the standard output to its original value
+
+window = properties.xs(12, level=0) # This retrieves only the data for the specified window_num
+window['Widths'] = window['Left/Right'].apply(lambda x: [*map(lambda y: y[1]-y[0], x)]) #This adds a new column to the window dataframe containing a list of all the widths of the modes.
+
+index = np.array(window.index.get_level_values(level=0).tolist())
+index = index.astype(float)
+
+percentages = (index-np.min(index))/(np.max(index)-np.min(index))
+locations = window['Peak'].tolist()
+widths = window['Widths'].tolist()
+
+pre_label_dataset=[]
+#pre_label_dataset[time_index]= [list(location), list(width)]
+
+for i in range(len(percentages)):
+	pre_label_dataset.append([locations[i],widths[i]])
+
+#print(percentages)
 
 
-print(a)
+post_label_dataset, total_band_num=data_labeler(pre_label_dataset,overlap_percent=50.)
+
+#post_label_dataset[time_index]= [list(location), list(width), list(label), list(band_type)]
+'''
+band_type:  0: constant band from the beginning
+            n0x: split, 101: parents label is 1, split once, 
+                        203, parents label is 3, split twice
+            2: new band
+'''
+band_labels=[]
+#band_labels=[band_label_number,time,info]
+
+#loop through all band numbers
+for i in (np.array(range(total_band_num))+1):
+	band_temp=[]
+	#band_temp[band_num]=[time_index,location, width, band_type]
+	#loop through all the time steps
+	for j in range(len(post_label_dataset)):
+		#loop through all the peak of this time step
+		for k in range(len(post_label_dataset[j][2])):
+			#if post_label_dataset = the current band loop index
+			if post_label_dataset[j][2][k]==i:
+				band_temp.append([index[j], 				#time step
+								percentages[j], 			#percent of the ELM
+								post_label_dataset[j][0][k],#Frequency(location of peak) 
+								post_label_dataset[j][1][k],#band width(width of peak) 
+								post_label_dataset[j][3][k] #band_type
+								])
+	band_labels.append(np.array(band_temp))
+
+#band_labels=np.array(band_labels)
+
+color_list=['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+
+#From https://stackabuse.com/writing-to-a-file-with-pythons-print-function/
+original_stdout = sys.stdout # Save a reference to the original standard output
+with open('band_labels.txt', 'w') as f:
+	sys.stdout = f # Change the standard output to the file we created.
+	#From: https://stackoverflow.com/questions/19124601/pretty-print-an-entire-pandas-series-dataframe
+	for i in range(total_band_num):
+		print('********'+str(i)+'******')
+		print('***************')
+		print('***************')
+		print('***************')
+		print('***************')
+		print('***************')
+		print(str(band_labels))
+	sys.stdout = original_stdout # Reset the standard output to its original value
+
+
+print('np.shape(band_labels[0])'+str(np.shape(band_labels[0])))
+
+total_band_num=5
+
+plt.clf()
+plt.plot()
+for i in range(total_band_num):
+	if len(np.shape(band_labels[i]))==2:
+		plt.plot(band_labels[i][:,0],band_labels[i][:,2],color=color_list[i%len(color_list)],marker='None',label='band '+str(i))
+plt.legend()
+#plt.title('Title',fontsize=20)
+plt.xlabel('time')
+plt.ylabel('frequency')
+plt.show()
+
+
+plt.clf()
+plt.plot()
+for i in range(total_band_num):
+	if len(np.shape(band_labels[i]))==2:
+		plt.plot(band_labels[i][:,1]*100.,band_labels[i][:,3],color=color_list[i%len(color_list)],marker='None',label='band '+str(i))
+plt.legend()
+#plt.title('Title',fontsize=20)
+plt.xlabel('Percent of ELM')
+plt.ylabel('Width of frequency band')
+plt.show()
--- SULI2021/random_tools/tools.py
+++ SULI2021/random_tools/tools.py
@@ -1,5 +1,6 @@
 from numpy import argmin, abs, array
 import numpy as np
+import copy
 
 #Modified by Max Curie 03/24/2021
 
@@ -20,7 +21,10 @@ def data_labeler(a,overlap_percent=50.):
     #a[time_index]= [list(location), list(width)]
     total_label=0
     b=[]
+    #loop through the time steps
     for i in range(len(a)):
+        print("at timestep "+str(i))
+        #for the first time step
         if i==0:
             location_temp=[]
             width_temp=[]
@@ -33,11 +37,15 @@ def data_labeler(a,overlap_percent=50.):
                 width_temp.append(a[i][j][1])
                 label_temp.append(j)
                 band_type_temp.append(0)
+        #for the rest of time steps
         else:
             location_temp=[]
             width_temp=[]
-            label_temp=[]
-            band_type_temp=[]
+            #(band_num,info_num)=np.shape(a[i])
+
+            #A new frequency band appears will be labeled -1
+            label_temp=[-1]*len(a[i])
+            band_type_temp=[0]*len(a[i])
             #loop throught all the bands for this time step
             for j in range(len(a[i])):
                 location_temp.append(a[i][j][0])
@@ -47,52 +55,68 @@ def data_labeler(a,overlap_percent=50.):
                 for m in range(len(b[-1][2])):
                     current_location=a[i][j][0]
                     current_width   =a[i][j][1]
-                    last_label      =b[-1][2][m]
-                    last_location   =b[-1][2][m]
+                    last_location   =b[-1][0][m]
                     last_width      =b[-1][1][m]
+                    last_label      =b[-1][2][m]
 
                     overlap_percent_calc=100.*abs(current_location-last_location)/\
                                     np.mean([current_width,last_width])
                     #Current frequency band
                     if (overlap_percent<=overlap_percent_calc):
-                        label_temp.append(last_label)
-                    #A new frequency band appears
-                    else: 
-                        label_temp.append(-1)
+                        label_temp[j]=last_label
+            #end of loop throught all the bands for this time step
 
-                label_count=np.zeros(len(b[-1][2]))
+            #start of labeling all the band type 2 and n0x 
+            label_count=np.zeros(int(np.max(label_temp))+2)
 
-                band_type_temp=np.zero(np.shape(label_temp))
+            print('label_temp'+str(label_temp))
 
-                for j in label_temp:
+            for j in label_temp:
+                if j==-1:
+                    #total number of new bands
+                    label_count[0]=label_count[0]+1
+                else:
                     label_count[j]=label_count[j]+1
 
-                label_counter=np.zeros(np.shape(label_count))
+            print('label_count: '+str(label_count))
+            #counter for the loop
+            label_counter=np.zeros(int(np.max(label_temp))+int(label_count[0])+1)
 
-                #loop through the label_list from current time step
-                for j in range(len(label_temp)):
-                    #constant band
-                    if label_count[label_temp[j]]==1:
-                        band_type_temp[j]=0
-                    #Current frequency band split
-                    elif label_count[label_temp[j]]>1 and label_temp[j]!=-1:
-                        band_type_temp[j]=100+label_temp[j]
-                        k=label_counter[label_temp[j]]
-                        #the first branch of the 
-                        if k==0:
-                            label_temp[j]=label_temp[j]
-                        else:
-                            total_label=total_label+1
-                            label_temp[j]=total_label
-                        label_counter[label_temp[j]]=label_counter[label_temp[j]]+1
-                    #A new independent frequency band appears
-                    if label_temp[j]==-1:
+            print('label_counter'+str(label_counter))
+
+            label_temp_copy=copy.deepcopy(label_temp)
+
+            #loop through the label_list from current time step
+            for j in range(len(label_temp)):
+                print('label_temp_copy'+str(label_temp_copy))
+                #constant band
+                if label_count[label_temp[j]]==1:
+                    band_type_temp[j]=0
+                #Current frequency band split
+                     #Multiple bands              and   not new bands
+                elif label_count[label_temp[j]]>1 and label_temp[j]!=-1:
+                    band_type_temp[j]=100+label_temp[j]
+                    k=label_counter[label_temp[j]]
+                    #the first branch of the 
+                    if k==0:
+                        label_temp[j]=label_temp[j]
+                    else:
                         total_label=total_label+1
                         label_temp[j]=total_label
-                        band_type_temp[j]=2
+                    print('label_temp_copy'+str(label_temp_copy))
+                    print('label_temp_copy[j]'+str(label_temp_copy[j]))
+                    print('j:'+str(j))
+                    label_counter[label_temp_copy[j]]=label_counter[label_temp_copy[j]]+1
+                    
+                #A new independent frequency band appears
+                if label_temp[j]==-1:
+                    total_label=total_label+1
+                    label_temp[j]=total_label
+                    band_type_temp[j]=2
 
-                print('label_count:'+str(label_count))
-                print('label_counter:'+str(label_counter))
+            print('label_count:'+str(label_count))
+            print('label_counter:'+str(label_counter))
+            #end of labeling all the band type 2 and n0x
 
         b.append([location_temp, width_temp, label_temp, band_type_temp])
 
@@ -103,4 +127,5 @@ def data_labeler(a,overlap_percent=50.):
                             203, parents label is 3, split twice
                 2: new band
     '''
-    return b
+
+    return b, total_label
